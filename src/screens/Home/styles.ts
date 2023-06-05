4f3d74df2b4619520f94c30b262cce09c2b3b3cc
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-06-05 14:25:51 -0300

    feat: use button

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -25,4 +25,16 @@ export const styles = StyleSheet.create({
       fontSize: 16,
       marginTop: 16,
     },
+    button: {
+      width: 56,
+      height: 56,
+      borderRadius: 5,
+      backgroundColor: '#31cf67',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    buttonText: {
+      color: '#FFF',
+      fontSize: 24,
+    },
   })
